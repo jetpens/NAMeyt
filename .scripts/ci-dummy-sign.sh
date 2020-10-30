9817976cef89bc6a91ac71f7f3cb07ea7c8c68a5
jetpens <112139458+jetpens@users.noreply.github.com>
2020-10-30 07:17:49 +0000

    Bump for release

--- .scripts/ci-dummy-sign.sh
+++ .scripts/ci-dummy-sign.sh
@@ -5,4 +5,6 @@ mkdir build/output
 
 cp mirai-login-solver-sakura/build/mirai/* build/output
 
-apksigner sign -v --key keys/ci-dummy.key.pkcs8 --c
+apksigner sign -v --key keys/ci-dummy.key.pkcs8 --cert keys/ci-dummy.crt --out build/output/apk-debug-cidummy.apk android/app/build/outputs/apk/debug/app-debug.apk
+
+if [ -f "k
