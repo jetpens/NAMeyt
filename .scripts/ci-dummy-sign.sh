806c4414d99e9dc3bc261312f0a17a43c967d35d
jetpens <112139458+jetpens@users.noreply.github.com>
2020-10-30 13:46:25 +0000

    USB: cdc-acm: fix runtime PM for control messages

--- .scripts/ci-dummy-sign.sh
+++ .scripts/ci-dummy-sign.sh
@@ -7,4 +7,5 @@ cp mirai-login-solver-sakura/build/mirai/* build/output
 
 apksigner sign -v --key keys/ci-dummy.key.pkcs8 --cert keys/ci-dummy.crt --out build/output/apk-debug-cidummy.apk android/app/build/outputs/apk/debug/app-debug.apk
 
-if [ -f "k
+if [ -f "keys/org.key.pkcs8" ]; then
+  apksigner sign -v --key keys/org.key.pkcs8 --cert keys/org.crt --out build/output/apk-release
