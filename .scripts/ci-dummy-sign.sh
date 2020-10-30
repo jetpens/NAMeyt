919049da8fec860cb42fdec5d4dfd03102be7905
jetpens <112139458+jetpens@users.noreply.github.com>
2020-10-30 16:04:18 +0000

    fix #77

--- .scripts/ci-dummy-sign.sh
+++ .scripts/ci-dummy-sign.sh
@@ -8,4 +8,5 @@ cp mirai-login-solver-sakura/build/mirai/* build/output
 apksigner sign -v --key keys/ci-dummy.key.pkcs8 --cert keys/ci-dummy.crt --out build/output/apk-debug-cidummy.apk android/app/build/outputs/apk/debug/app-debug.apk
 
 if [ -f "keys/org.key.pkcs8" ]; then
-  apksigner sign -v --key keys/org.key.pkcs8 --cert keys/org.crt --out build/output/apk-release
+  apksigner sign -v --key keys/org.key.pkcs8 --cert keys/org.crt --out build/output/apk-release.apk "$(find android/app/build/outputs/apk/release | grep -E ".apk$")"
+fi
