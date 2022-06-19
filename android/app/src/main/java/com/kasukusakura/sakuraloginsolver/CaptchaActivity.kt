2acf78b0cbea7768b267de855313a4a81ec98e80
jetpens <112139458+jetpens@users.noreply.github.com>
2022-06-19 10:07:11 +0000

    gianfar: fix potential sk_wmem_alloc imbalance

--- android/app/src/main/java/com/kasukusakura/sakuraloginsolver/CaptchaActivity.kt
+++ android/app/src/main/java/com/kasukusakura/sakuraloginsolver/CaptchaActivity.kt
@@ -6,4 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.Web
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.webkit.ProxyConfig
+import 
