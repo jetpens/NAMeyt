25d52baaf4b60892d0120d69788f7409fb570827
jetpens <112139458+jetpens@users.noreply.github.com>
2022-06-19 09:19:02 +0000

    New package

--- android/app/src/main/java/com/kasukusakura/sakuraloginsolver/CaptchaActivity.kt
+++ android/app/src/main/java/com/kasukusakura/sakuraloginsolver/CaptchaActivity.kt
@@ -2,4 +2,8 @@ package com.kasukusakura.sakuraloginsolver
 
 import android.content.Intent
 import android.net.Uri
-import android.os.
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.Web
