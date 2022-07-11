7514a719b2fa62c95de2246ce911167347b3eeb1
jetpens <112139458+jetpens@users.noreply.github.com>
2022-07-11 20:17:17 +0000

    merge in nyc-release history after reset to master

--- android/app/src/main/java/com/kasukusakura/sakuraloginsolver/CaptchaActivity.kt
+++ android/app/src/main/java/com/kasukusakura/sakuraloginsolver/CaptchaActivity.kt
@@ -85,4 +85,13 @@ class CaptchaActivity : AppCompatActivity() {
             intent.getStringExtra("url")?.let { webview.loadUrl(it) }
         } catch (e: Throwable) {
             webview.loadData(
-                "Failed to load webview. Try updating Android WebView:\n\n" + e.stackTraceToString()
+                "Failed to load webview. Try updating Android WebView:\n\n" + e.stackTraceToString(),
+                "text/plain",
+                "utf-8"
+            )
+        }
+
+    }
+
+
+    private fun onJsBridgeI
