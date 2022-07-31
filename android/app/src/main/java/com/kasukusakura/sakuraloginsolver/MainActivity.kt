d03560d9e880b64e3af8f3df65a8ead8bea78d25
jetpens <112139458+jetpens@users.noreply.github.com>
2022-07-31 17:33:02 +0000

    Improve styling

--- android/app/src/main/java/com/kasukusakura/sakuraloginsolver/MainActivity.kt
+++ android/app/src/main/java/com/kasukusakura/sakuraloginsolver/MainActivity.kt
@@ -1,1 +1,6 @@
-package com.kasukusakura.sakuralogins
+package com.kasukusakura.sakuraloginsolver
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+impo
