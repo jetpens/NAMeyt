651f0a4c493d08af8264db1a96e21f92e2bc1e21
jetpens <112139458+jetpens@users.noreply.github.com>
2022-08-13 07:30:36 +0000

    Added migrations.

--- android/app/src/main/java/com/kasukusakura/sakuraloginsolver/MainActivity.kt
+++ android/app/src/main/java/com/kasukusakura/sakuraloginsolver/MainActivity.kt
@@ -16,4 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-     
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.rightLeft.text = "Version: " + BuildConfig.
