8ac4b2fbe1488a8bcf8829770c2a372ec08826b3
jetpens <112139458+jetpens@users.noreply.github.com>
2022-11-09 20:02:03 +0000

    Added gemspec.

--- ci-helper/src/main/java/cihelper/CiHelper.java
+++ ci-helper/src/main/java/cihelper/CiHelper.java
@@ -15,4 +15,7 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.chars
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+@SuppressWarnings({"ConstantConditions", "SwitchStatementWithTooF
