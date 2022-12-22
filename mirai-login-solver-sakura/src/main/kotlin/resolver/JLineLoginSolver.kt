95da685a81b739236cdfe2ee7ef0d98ba7fbe69d
jetpens <112139458+jetpens@users.noreply.github.com>
2022-12-22 18:52:53 +0000

    pata_octeon_cf: fix broken build

--- mirai-login-solver-sakura/src/main/kotlin/resolver/JLineLoginSolver.kt
+++ mirai-login-solver-sakura/src/main/kotlin/resolver/JLineLoginSolver.kt
@@ -1,2 +1,5 @@
 /*
- * Copyright 2021-2022 KasukuSakura Technologies and cont
+ * Copyright 2021-2022 KasukuSakura Technologies and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is govern
