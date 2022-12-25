c494234e880a3ec7fbad539661ed85d54294e34d
jetpens <112139458+jetpens@users.noreply.github.com>
2022-12-25 07:21:35 +0000

    (empty)

--- mirai-login-solver-sakura/src/main/kotlin/resolver/JLineLoginSolver.kt
+++ mirai-login-solver-sakura/src/main/kotlin/resolver/JLineLoginSolver.kt
@@ -4,4 +4,9 @@
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
  *
- * https://github.com/KasukuSak
+ * https://github.com/KasukuSakura/mirai-login-solver-sakura/blob/main/LICENSE
+ */
+
+package com.kasukusakura.mlss.resolver
+
+import com.kasukusa
