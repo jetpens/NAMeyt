e767fd6554e0aa6768598868bd2557a6112cf130
jetpens <112139458+jetpens@users.noreply.github.com>
2023-01-10 09:14:17 +0000

    Clean up white space.

--- mirai-login-solver-sakura/src/main/kotlin/resolver/QRCode.kt
+++ mirai-login-solver-sakura/src/main/kotlin/resolver/QRCode.kt
@@ -10,4 +10,6 @@
 package com.kasukusakura.mlss.resolver
 
 import com.google.zxing.BarcodeFormat
-import com.googl
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.google.zxing.common.BitMatrix
+import com.google.zxing.qrcode.Q
