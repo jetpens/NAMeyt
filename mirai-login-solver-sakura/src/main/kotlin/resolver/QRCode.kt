6a85c05b95587ece0a7233cb50e3111dbdffd248
jetpens <112139458+jetpens@users.noreply.github.com>
2023-01-10 20:56:15 +0000

    doesn't work

--- mirai-login-solver-sakura/src/main/kotlin/resolver/QRCode.kt
+++ mirai-login-solver-sakura/src/main/kotlin/resolver/QRCode.kt
@@ -12,4 +12,7 @@ package com.kasukusakura.mlss.resolver
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.common.BitMatrix
-import com.google.zxing.qrcode.Q
+import com.google.zxing.qrcode.QRCodeWriter
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
+import com.google.zxing.qrcode.encoder.QRCode
+impor
